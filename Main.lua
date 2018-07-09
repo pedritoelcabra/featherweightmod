abc2b6e094768ca2aa07eea811ef4775a9266b4e
pedrito_elcabra <pedrito_elcabra@hotmail.com>
2018-07-09 19:08:50 +0100

    Reveal 4 tiles around spawn

--- Main.lua
+++ Main.lua
@@ -14,22 +14,22 @@ local function SetInitialVisibility()
 	-- Apply updates to all majors
 	local aPlayers = PlayerManager.GetAliveMajors();
 	for loop, pPlayer in ipairs(aPlayers) do
-		if(pPlayer:IsHuman() == true) then
-			local iPlayer = pPlayer:GetID();
-
-			local startingPlot = pPlayer:GetStartingPlot();
-
-			local startX = startingPlot:getX() - 4;
-			local startY = startingPlot:getY() - 4;
-			local endX = startingPlot:getX() + 4;
-			local endY = startingPlot:getY() + 4;
 	
-			local pCurPlayerVisibility = PlayersVisibility[pPlayer:GetID()];
-			if(pCurPlayerVisibility ~= nil) then
-		
-				-- Reveal Space Around Spawn
-				for iX = startX, endX, 1 do
-					for iY = startY, endY, 1 do
+		local iPlayer = pPlayer:GetID();
+
+		local startingPlot = pPlayer:GetStartingPlot();
+
+		g_iW, g_iH = Map.GetGridSize();
+
+		local pCurPlayerVisibility = PlayersVisibility[pPlayer:GetID()];
+		if(pCurPlayerVisibility ~= nil) then
+	
+			-- Reveal Space Around Spawn
+			for iX = 0, g_iW - 1 do
+				for iY = 0, g_iH - 1 do
+					
+					local iDistance = Map.GetPlotDistance(startingPlot:GetX(), startingPlot:GetY(), iX, iY);
+					if (iDistance < 5) then
 						local iPlotIndex = Map.GetPlot(iX, iY):GetIndex();
 						pCurPlayerVisibility:ChangeVisibilityCount(iPlotIndex, 1);
 					end
